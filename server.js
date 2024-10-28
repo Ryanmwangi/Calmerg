26c4997af099ded6235077aaf3fa46585870fa07
Ryan Mwangi <ryannganga13325@gmail.com>
2024-10-28 20:34:08 +0300

    delete unused crypto import

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ import ical from 'ical';
 import fs from 'fs';
 import cron from 'node-cron';
 import axios from 'axios';
-import crypto from 'crypto';
 import path from 'path';
 
 const app = express();
